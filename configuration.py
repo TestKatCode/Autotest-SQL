58bd164c49f653571072fb8188f740289f7074c3
TestKatCode <79061719499@yandex.ru>
2023-11-14 21:50:29 +0300

    Add files

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://46ac0320-14da-4efe-89aa-b472472a4aa7.serverhub.praktikum-services.ru" # адрес сервера
+CREATE_COURIER = "/api/v1/courier"   # создание курьера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # информация о заказе по его номеру
